fe522d94d2c04260ec3d839690a8211d60ebf83a
Jakub Kleszko <jakub.kleszko@pollub.edu.pl>
2022-10-19 19:03:15 +0200

    Wykonanie Zadania 2 - wersja 1

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -3,14 +3,14 @@ package pwo;
 public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
-    public static final String FILE_NAME = "fibseq100.txt";
+    public static final String FILE_NAME = "luceq100.txt";
 
     public static void main(String[] args) {
+        
+        System.out.println("Lucas wersja 1");
 
-        System.out.println("Fibonacci wersja 2");
-
-        boolean ok = SequenceTools.writeToFile(
-                new FibonacciGenerator(), FROM, TO, FILE_NAME);
+        boolean ok = SequenceToolsForLucas.writeToFile(
+                new LucasGenerator(), FROM, TO, FILE_NAME);
 
         if (ok) {
             System.out.println("Wynik zapisany do pliku: "
@@ -18,5 +18,16 @@ public class Zadanie1 {
         } else {
             System.out.println("Błąd");
         }
+//        System.out.println("Fibonacci wersja 2");
+//
+//        boolean ok = SequenceTools.writeToFile(
+//                new FibonacciGenerator(), FROM, TO, FILE_NAME);
+//
+//        if (ok) {
+//            System.out.println("Wynik zapisany do pliku: "
+//                    + FILE_NAME);
+//        } else {
+//            System.out.println("Błąd");
+//        }
     }
 }
